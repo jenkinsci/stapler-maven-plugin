0431efe450cb1bceb34ab9932e05d348f3196cf4
kohsuke <kohsuke@@dev.java.net>
2007-04-15 04:11:37 +0000

    needs to kick in even if there's no annotation.

--- maven-stapler-plugin/src/main/java/org/kohsuke/stapler/AnnotationProcessorFactoryImpl.java
+++ maven-stapler-plugin/src/main/java/org/kohsuke/stapler/AnnotationProcessorFactoryImpl.java
@@ -1,18 +1,14 @@
 package org.kohsuke.stapler;
 
-import com.sun.mirror.apt.AnnotationProcessorFactory;
 import com.sun.mirror.apt.AnnotationProcessor;
 import com.sun.mirror.apt.AnnotationProcessorEnvironment;
+import com.sun.mirror.apt.AnnotationProcessorFactory;
 import com.sun.mirror.apt.AnnotationProcessors;
 import com.sun.mirror.declaration.AnnotationTypeDeclaration;
 
 import java.util.Collection;
-import java.util.Set;
 import java.util.Collections;
-import java.util.Arrays;
-
-import org.kohsuke.stapler.export.ExposedBean;
-import org.kohsuke.stapler.export.Exposed;
+import java.util.Set;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -24,7 +20,7 @@ final class AnnotationProcessorFactoryImpl implements AnnotationProcessorFactory
     }
 
     public Collection<String> supportedAnnotationTypes() {
-        return Arrays.asList(ExposedBean.class.getName(), Exposed.class.getName(), DataBoundConstructor.class.getName());
+        return Collections.singleton("*");
     }
 
     public AnnotationProcessor getProcessorFor(Set<AnnotationTypeDeclaration> set, AnnotationProcessorEnvironment env) {

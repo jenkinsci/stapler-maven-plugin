bf8c449826204c9dc4b738da829b60d8a326a634
null <null@@dev.java.net>
2008-01-02 17:23:54 +0000

    This commit was manufactured by cvs2svn to create tag 'maven-stapler-plugin-1_5'.

--- stapler/release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -ex
-mvn -B release:prepare release:perform
-ver=$(show-pom-version target/checkout/pom.xml)
-javanettasks uploadFile stapler /stapler-$ver.zip "$ver release" stable target/checkout/target/stapler-$ver-bin.zip
-./push-javadoc.sh
